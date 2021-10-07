c6a12eb045e5f9fc601cbd0a0393d979efa1bc90
yuki1567 <89760110+yuki1567@users.noreply.github.com>
2021-10-07 17:46:32 +0900

    userモデルのバリデーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  with_options presence: true do
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' } do
+      validates :name
+    end
+    with_options format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters' } do
+      validates :kana_name
+    end
+  end
 end
