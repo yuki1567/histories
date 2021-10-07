983fc2824a61a5cb6074d2398cee3c94a28c446d
yuki1567 <89760110+yuki1567@users.noreply.github.com>
2021-10-07 18:01:24 +0900

    本登録機能の実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,29 @@
 class BooksController < ApplicationController
+  before_action :user_admin, only: [:new]
+
   def index
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:image, :title, :author, :content, :quantity, :category_id)
+  end
+
+  def user_admin
+    redirect_to root_path unless user_signed_in? && current_user.admin?
   end
 end
