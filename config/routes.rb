2f4b935ac6ea8dd7acef5be44e58e1f32e12f0c3
yuki1567 <89760110+yuki1567@users.noreply.github.com>
2021-10-07 17:22:34 +0900

    deviseのルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'books/index'
+  devise_for :users, controllers: { 
+    registrations: 'users/registrations',
+    sessions: 'users/sessions' 
+  }
   root to: 'books#index'
 end
