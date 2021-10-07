8d7ed15fefd3e16edab6d8afb71d4d0144b59f45
yuki1567 <89760110+yuki1567@users.noreply.github.com>
2021-10-07 18:00:59 +0900

    バリデーションの記述

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,16 @@
 class Book < ApplicationRecord
+  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  with_options presence: true do
+    validates :image
+    validates :title
+    validates :author
+    validates :content
+    validates :category_id
+    with_options numericality: { only_integer: true, greater_than_or_equal_to: 0,
+                                 message: 'is invalid. Input half-width numbers' } do
+      validates :quantity
+    end
+  end
 end
