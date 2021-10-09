e1e0083bbf1bcee822e619df7928d5d125aacef7
yuki1567 <89760110+yuki1567@users.noreply.github.com>
2021-10-09 16:37:15 +0900

    ルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   }
   root to: 'books#index'
   resources :books
+  resources :borrows, only: [:new, :create]
   resources :users, only: [:index, :show] do
     resources :carts, only: [:show, :create, :destroy]
   end
