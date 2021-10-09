c01251452db2c7172db2462f991bcdd2213f3778
yuki1567 <89760110+yuki1567@users.noreply.github.com>
2021-10-09 16:59:31 +0900

    ユーザー一覧機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
+  before_action :user_admin, only: [:index]
+
   def index
+    @users = User.all
   end
   
   def show
+  end
+
+  def user_admin
+    redirect_to root_path unless user_signed_in? && current_user.admin?
   end
 end
