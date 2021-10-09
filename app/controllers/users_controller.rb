0635fa9448a62104bf678b2c1b83ea083795c9dd
yuki1567 <89760110+yuki1567@users.noreply.github.com>
2021-10-09 17:28:55 +0900

    マイページ機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   before_action :user_admin, only: [:index]
+  before_action :authenticate_user!, only: [:show]
+  before_action :move_to_index, only: [:show]
+
 
   def index
     @users = User.all
@@ -10,5 +13,10 @@ class UsersController < ApplicationController
 
   def user_admin
     redirect_to root_path unless user_signed_in? && current_user.admin?
+  end
+
+  def move_to_index
+    @user = User.find(params[:id])
+    redirect_to root_path unless current_user.admin? || @user.id == current_user.id
   end
 end
