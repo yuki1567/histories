06d19bb1eb8009813b57681adc2a70efb2e73650
yuki1567 <89760110+yuki1567@users.noreply.github.com>
2021-10-09 16:38:53 +0900

    カート機能の実装

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,17 +3,24 @@ class CartsController < ApplicationController
   before_action :set_cart, only: [:show, :destroy]
 
   def show
-    @cart_books = @cart.cart_books.includes(:cart).order('created_at DESC')
+    @cart_books = @cart.cart_books.includes(:cart)
   end
 
   def create
-    cart = current_user.cart
-    cart.quantity += params[:quantity].to_i
-    if cart.save
-      CartBook.create(cart_book_params)
-      redirect_to root_path
+    book = Book.find(params[:book_id])
+    unless current_user.cart.cart_books.where(book_id: book.id).present?
+      cart = current_user.cart
+      cart.quantity += params[:quantity].to_i
+      if cart.save
+        CartBook.create(cart_book_params)
+        redirect_to root_path
+      else
+        @book = Book.find(params[:book_id])
+        render template: 'books/show' and return
+      end
     else
       @book = Book.find(params[:book_id])
+      flash.now[:danger] = "すでに同じ本がカートの中に入っています"
       render template: 'books/show'
     end
   end
