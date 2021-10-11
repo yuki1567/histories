da21743cb71421510020cf6313abcdb1045b67e6
yuki1567 <89760110+yuki1567@users.noreply.github.com>
2021-10-11 14:28:23 +0900

    booksコントローラーのcreateアクションの単体テスト

--- spec/requests/books_request_spec.rb
+++ spec/requests/books_request_spec.rb
@@ -3,6 +3,14 @@ require 'rails_helper'
 describe BooksController, type: :request do
   let(:admin) { FactoryBot.create(:user, :a) }
   let(:user) { FactoryBot.create(:user, :b) }
+  let(:book) { FactiryBot.create(:book) }
+  let(:book_params) do
+    { book: { image: fixture_file_upload('files/test_image.jpeg'), title: 'test', author: 'test', content: 'test',
+      quantity: '1', category_id: '1' } }
+  end
+  let(:invalid_book_params) do
+    { book: { title: "" } }
+  end
 
   describe 'GET #new' do
     context '管理者ユーザーでログインした場合' do
@@ -39,6 +47,38 @@ describe BooksController, type: :request do
       it 'トップページに遷移すること' do
         get new_book_path
         expect(response).to redirect_to root_path
+      end
+    end
+  end
+
+  describe 'POST #create' do
+    before do
+      sign_in(admin)
+    end
+    context '保存に成功した場合' do
+      it 'createアクションにレスポンスすると正常にレスポンスが返ってきている' do
+        post books_path, params: book_params
+        expect(response.status).to eq 302
+      end
+      it 'データベースに保存ができた' do
+        expect { post books_path, params: book_params }.to change(Book, :count).by(1)
+      end
+      it 'トップページに遷移すること' do
+        post books_path, params: book_params
+        expect(response).to redirect_to root_path
+      end
+    end
+    context '保存に失敗した場合' do
+      it 'createアクションにレスポンスすると正常にレスポンスが返ってきていない' do
+        post books_path, params: invalid_book_params
+        expect(response.status).not_to eq 302
+      end
+      it 'データベースに保存ができていない' do
+        expect { post books_path, params: invalid_book_params }.not_to change(Book, :count)
+      end
+      it 'エラーメッセージが表示されている' do
+        post books_path, params: invalid_book_params
+        expect(response.body).to include('error-message')
       end
     end
   end
