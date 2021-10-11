5be98204180eff72c779dbff70736e02284691d4
yuki1567 <89760110+yuki1567@users.noreply.github.com>
2021-10-11 17:10:30 +0900

    cartモデルの単体テスト

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:cart) { FactoryBot.build(:cart) }
+
+  describe 'カートに本の追加' do
+    context '本の追加ができる場合' do
+      it 'quantityが９以下なら本の追加ができる' do
+        cart.quantity = 9
+        expect(cart).to be_valid
+      end
+    end
+
+    context '本の追加ができない場合' do
+      it 'quantityが11以上になるなら本の追加ができない' do
+        cart.quantity = 11
+        cart.valid?
+        expect(cart.errors.full_messages).to include('Quantity is full')
+      end
+
+      it 'userが紐付いていないと本の追加ができない' do
+        cart.user = nil
+        cart.valid?
+        expect(cart.errors.full_messages).to include('User must exist')
+      end
+    end
+  end
 end
