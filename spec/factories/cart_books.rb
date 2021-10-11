ae7475c61b900ab1f64a0fb3b1a74940e8a61ff9
yuki1567 <89760110+yuki1567@users.noreply.github.com>
2021-10-11 23:00:31 +0900

    cartモデルの単体テスト

--- spec/factories/cart_books.rb
+++ spec/factories/cart_books.rb
@@ -1,4 +1,6 @@
 FactoryBot.define do
   factory :cart_book do
+    association :cart
+    association :book
   end
 end
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -4,21 +4,21 @@ RSpec.describe Cart, type: :model do
   let(:cart) { FactoryBot.build(:cart) }
 
   describe 'カートに本の追加' do
-    context '本の追加ができる場合' do
-      it 'quantityが９以下なら本の追加ができる' do
+    context 'カートに本の追加ができる場合' do
+      it 'quantityが９以下ならカートに本の追加ができる' do
         cart.quantity = 9
         expect(cart).to be_valid
       end
     end
 
-    context '本の追加ができない場合' do
-      it 'quantityが11以上になるなら本の追加ができない' do
+    context 'カートに本の追加ができない場合' do
+      it 'quantityが11以上になるならカートに本の追加ができない' do
         cart.quantity = 11
         cart.valid?
         expect(cart.errors.full_messages).to include('Quantity is full')
       end
 
-      it 'userが紐付いていないと本の追加ができない' do
+      it 'userが紐付いていないとカートに本の追加ができない' do
         cart.user = nil
         cart.valid?
         expect(cart.errors.full_messages).to include('User must exist')
