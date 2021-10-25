b362c51300b5859cd3eddc66cafe7fd0e29c5303
yuki1567 <89760110+yuki1567@users.noreply.github.com>
2021-10-25 17:43:54 +0900

    commentsコントローラーのcreateアクションの単体テスト

--- spec/requests/comments_request_spec.rb
+++ spec/requests/comments_request_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "Comments", type: :request do
+  let(:book) { FactoryBot.create(:book) }
+  let(:comment_params) do
+    { comment: { text: 'test', book_id: book.id } }
+  end
 
+  describe 'POST #create' do
+    it 'createアクションにレスポンスすると正常にレスポンスが返ってきている' do
+      post book_comments_path(book), params: comment_params
+      expect(response.status).to eq 204
+    end
+    it 'データベースに保存ができている' do
+      expect { post book_comments_path(book), params: comment_params }.to change(Comment, :count).by(1)
+    end
+  end
 end
