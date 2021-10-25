96bfcf7e924a79f9cdb9054401a55e5539d2ac90
yuki1567 <89760110+yuki1567@users.noreply.github.com>
2021-10-25 17:43:09 +0900

    commentモデルの単体テスト

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :comment do
+    text { Faker::Lorem.sentence }
     
+    association :book
   end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:comment) { FactoryBot.build(:comment) }
+
+  describe 'コメント' do
+    it 'bookが紐付いていればコメントできる' do
+      expect(comment).to be_valid
+    end
+    it 'bookが紐付いていないとコメントできない' do
+      comment.book = nil
+      comment.valid?
+      expect(comment.errors).to be_added(:book, :blank)
+    end
+  end
 end
